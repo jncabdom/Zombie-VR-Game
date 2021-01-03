12d44961d40fe6d205c8a216b8c5eeb9eca05123
ChristianJPH <43588689+ChristianJPH@users.noreply.github.com>
2021-01-03 12:47:05 +0000

    Se puede comprar un arma de la pared y aparece en en 0,1,0

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Here I'm defining health, cash...
+public class PlayerStats : MonoBehaviour
+{
+  public static float health = 100;
+  public static int money = 400;
+
+  private void Start() {
+    WeaponManager.OnDecreaseMoney += DecreaseMoney;
+    WeaponManager.OnEnoughMoney += EnoughMoney;
+  }
+
+  // If enough money then decrease it and return a success state
+  void DecreaseMoney(int price) {
+    money -= price;
+  }
+
+  bool EnoughMoney(int price) {
+    return (money >= price)? true : false;
+  }
+
+  private void OnDisable() {
+    WeaponManager.OnDecreaseMoney -= DecreaseMoney;
+    WeaponManager.OnEnoughMoney -= EnoughMoney;
+  }
+
+  // Update is called once per frame
+  void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/WeaponManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate bool EnoughMoney(int price);
+public delegate void DecreaseMoney(int price);
+
+public class WeaponManager : MonoBehaviour
+{
+  public float radius = 2f;
+  public GameObject player;
+  public GameObject weapon;
+  public int price = 100;
+  public bool insideBuyingArea = false;
+  public static event DecreaseMoney OnDecreaseMoney;
+  public static event EnoughMoney OnEnoughMoney;
+
+  // Start is called before the first frame update
+  void Start() {
+        
+  }
+
+  // Update is called once per frame
+  void Update() {
+    float distance = Vector3.Distance(transform.position, player.transform.position);
+    if (distance < radius && OnEnoughMoney(price)) {
+      if (Input.GetButtonDown("Action")) {
+        OnDecreaseMoney(price);
+        AttachWeapon();
+      }
+          GameObject.Find("Canvas").GetComponent<Canvas>().transform.Find("BuyWeapon").gameObject.SetActive(true);
+        } else {
+          GameObject.Find("Canvas").GetComponent<Canvas>().transform.Find("NoMoney").gameObject.SetActive(true);
+        }
+      insideBuyingArea = true;
+    if (distance > radius) {
+      insideBuyingArea = false;
+      GameObject.Find("Canvas").GetComponent<Canvas>().transform.Find("BuyWeapon").gameObject.SetActive(false);
+      GameObject.Find("Canvas").GetComponent<Canvas>().transform.Find("NoMoney").gameObject.SetActive(false);
+      Debug.Log(PlayerStats.money);
+    }
+  }
+  void AttachWeapon() {
+    Instantiate(weapon, new Vector3(0, 2, 0), Quaternion.identity);
+  }
+}
