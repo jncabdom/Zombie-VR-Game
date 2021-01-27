7879df5522d89aa2d76c92c2d9daa29937569d71
Christian <christian173p@gmail.com>
2021-01-27 13:35:51 +0000

    AI zombie

---
+++ Assets/Scripts/MoveTowardsPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveTowardsPlayer : MonoBehaviour
+{
+    public Transform player;
+    public float deathDistance = 0.5f;
+    private NavMeshAgent navComponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navComponent = gameObject.GetComponent<NavMeshAgent>();
+        //navComponent.destination = player.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = Vector3.Distance(player.position, transform.position);
+        navComponent.destination = player.position;
+        if (dist < deathDistance) {
+            //matalo puta
+        }
+    }
+}
