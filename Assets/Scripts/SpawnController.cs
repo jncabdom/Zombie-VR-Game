310c45d09f2767e21954bcdee472d694a70f6e50
Nicolás Cabrera <jncabdom@gmail.com>
2021-01-29 12:20:08 +0000

    Actualizado script de spawn

--- Assets/Scripts/SpawnController.cs
+++ Assets/Scripts/SpawnController.cs
@@ -7,7 +7,7 @@ public class SpawnController : MonoBehaviour
     public GameObject songui;
     public Vector3 spawnValues;
 
-    void spawn()
+    public void spawn()
     {
         Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), 1, Random.Range (-spawnValues.z, spawnValues.z));
         Instantiate (songui, spawnPosition + transform.TransformPoint (0, 0 ,0), gameObject.transform.rotation);
