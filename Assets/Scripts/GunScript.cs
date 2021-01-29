0d8ae1606520c6c3b4f1d731782e8c3f72e4fdc4
Nicolás Cabrera <jncabdom@gmail.com>
2021-01-29 15:47:29 +0000

    Disparar con mando, pruebas de VR

--- Assets/Scripts/GunScript.cs
+++ Assets/Scripts/GunScript.cs
@@ -1,9 +1,13 @@
+using System.Collections;
 using UnityEngine;
 
 public class GunScript : MonoBehaviour
 {
     public float damage = 10f;
     public float range = 100f;
+    public float cadence = 3.0f;
+
+    bool canShoot = true;
 
     public Camera fpsCam;
     public ParticleSystem muzzleFlash;
@@ -17,15 +21,23 @@ public class GunScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire")) {
-            audioAk[0].Play();
-            shoot();
+        if ((Input.GetButtonDown("Fire")) || (Input.GetAxisRaw("FireController") == 1)) {
+            if(canShoot) {
+                StartCoroutine(shootWait());
+            }
         }
-        
+    }
+
+    IEnumerator shootWait() {
+        canShoot = false;
+        shoot();
+        yield return new WaitForSeconds(cadence);
+        canShoot = true;
     }
 
     void shoot() {
         muzzleFlash.Play();
+        audioAk[0].Play();
 
         RaycastHit hitInfo;
        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward,out hitInfo, range)) {
