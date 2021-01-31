3802f65fae3151b818b22ca0b8b3dec941967181
Nicolás Cabrera <jncabdom@gmail.com>
2021-01-31 12:20:47 +0000

    Restored scene, soldier head

---
+++ Assets/Scripts/SoldierHead.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoldierHead : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(new Vector3(Input.acceleration.y, Input.acceleration.x, 0));
+        transform.Rotate(Input.acceleration * Time.deltaTime);
+    }
+}
