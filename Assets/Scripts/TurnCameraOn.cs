86ec51a450a93729b1ff9c090abeeba3be40023c
Christian Pérez <43588689+ChristianJPH@users.noreply.github.com>
2021-01-31 13:53:18 +0000

    Una tele que no enciende

---
+++ Assets/Scripts/TurnCameraOn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnCameraOn : MonoBehaviour {
+
+    // if The user authorize to use the camera then we turn the camera on
+    IEnumerator Start() {
+        yield return Application.RequestUserAuthorization(UserAuthorization.WebCam);
+        if (Application.HasUserAuthorization(UserAuthorization.WebCam)) {
+            PlayCamera();
+        }
+        else {
+            Debug.Log("webcam not found");
+        }
+    }
+
+    void PlayCamera() {
+        WebCamTexture webcamTexture = new WebCamTexture();
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = webcamTexture;
+        webcamTexture.Play();
+    }
+}
