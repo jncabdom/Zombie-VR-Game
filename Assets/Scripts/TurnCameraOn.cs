79ad0f9a1ee5a7daded4b0a3620416f0359eacb9
Nicolás Cabrera <jncabdom@gmail.com>
2021-01-31 14:07:07 +0000

    TV working properly

--- Assets/Scripts/TurnCameraOn.cs
+++ Assets/Scripts/TurnCameraOn.cs
@@ -3,21 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TurnCameraOn : MonoBehaviour {
+    Renderer renderer;
+    WebCamTexture webcamTexture;
 
     // if The user authorize to use the camera then we turn the camera on
-    IEnumerator Start() {
-        yield return Application.RequestUserAuthorization(UserAuthorization.WebCam);
-        if (Application.HasUserAuthorization(UserAuthorization.WebCam)) {
-            PlayCamera();
-        }
-        else {
-            Debug.Log("webcam not found");
-        }
-    }
-
-    void PlayCamera() {
-        WebCamTexture webcamTexture = new WebCamTexture();
-        Renderer renderer = GetComponent<Renderer>();
+    void Start() {
+        renderer = GetComponent<Renderer>();
+        webcamTexture = new WebCamTexture();
         renderer.material.mainTexture = webcamTexture;
         webcamTexture.Play();
     }
