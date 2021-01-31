e1d22bdf19d2b3997feb92a1c0082ae0f6c29e60
Christian Pérez <43588689+ChristianJPH@users.noreply.github.com>
2021-01-31 13:56:37 +0000

    SIn permiso

--- Assets/Scripts/TurnCameraOn.cs
+++ Assets/Scripts/TurnCameraOn.cs
@@ -6,13 +6,7 @@ public class TurnCameraOn : MonoBehaviour {
 
     // if The user authorize to use the camera then we turn the camera on
     IEnumerator Start() {
-        yield return Application.RequestUserAuthorization(UserAuthorization.WebCam);
-        if (Application.HasUserAuthorization(UserAuthorization.WebCam)) {
-            PlayCamera();
-        }
-        else {
-            Debug.Log("webcam not found");
-        }
+        PlayCamera();
     }
 
     void PlayCamera() {
